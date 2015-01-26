59594906a74a00832e41f2748c61aba202b0a5ec
kami <kami30k@gmail.com>
2015-01-26 17:23:30 +0900

    Enable configuration

--- app/mailers/meyasubako/meyasubako_mailer.rb
+++ app/mailers/meyasubako/meyasubako_mailer.rb
@@ -5,7 +5,11 @@ module Meyasubako
     def email(content)
       @content = content
 
-      mail(to: 'kami30k@gmail.com', from: 'kami30k@gmail.com')
+      mail(
+        to:      Meyasubako.config.to,
+        from:    Meyasubako.config.from,
+        subject: I18n.t('meyasubako.subject')
+      )
     end
   end
 end
--- lib/meyasubako.rb
+++ lib/meyasubako.rb
@@ -1,1 +1,15 @@
 require 'meyasubako/engine'
+
+module Meyasubako
+  autoload :Config, 'meyasubako/config'
+
+  class << self
+    def configure
+      yield config
+    end
+
+    def config
+      @config ||= Config.new
+    end
+  end
+end
