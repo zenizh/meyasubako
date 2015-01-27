2adee23b8d3e7d741650f96da3de5d892f564505
kami <hiroki.zenigami@gmail.com>
2015-01-27 14:57:38 +0900

    Add integration test

---
+++ spec/requests/meyasubako_pages_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe 'Integration test', type: :feature, js: true do
+  let(:container) { find('.meyasubako__container', visible: false) }
+  let(:heading)   { find('.meyasubako__heading') }
+
+  before { visit root_path }
+
+  describe 'toggle box' do
+    it 'should toggle the visibility' do
+      expect(container).not_to be_visible
+
+      heading.click
+
+      expect(container).to be_visible
+    end
+  end
+
+  describe 'send opinion' do
+    it 'should send correctly' do
+      heading.click
+
+      fill_in :meyasubako__content, with: 'foo'
+
+      click_on 'Submit'
+
+      # Wait for sending email
+      expect(page).to have_content 'Sending ...'
+      expect(page).to have_content 'Thank you for sending your opinion.'
+
+      open_email 'username@example.com'
+
+      expect(current_email).to have_content 'foo'
+    end
+  end
+end
